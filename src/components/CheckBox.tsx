7701e6d3545e1330c8c25570161e86c99a916c99
angelon <angelon.gabriel@gmail.com>
2022-10-15 23:13:07 -0300

    Formulario criado

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,11 +1,11 @@
 import * as CheckBoxPrimitive from '@radix-ui/react-checkbox'
 import {Check} from 'phosphor-react'
 
-export interface CheckBoxProps{}
+export interface CheckBoxProps extends CheckBoxPrimitive.CheckboxProps{}
 
-export function CheckBox({ }:CheckBoxProps){
+export function CheckBox(props :CheckBoxProps){
     return(
-        <CheckBoxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" >
+        <CheckBoxPrimitive.Root {...props} className="w-6 h-6 p-[2px] bg-gray-800 rounded" >
             <CheckBoxPrimitive.Indicator asChild>
                 <Check weight="bold" className="h-5 w-5 text-cyan-500" />
             </CheckBoxPrimitive.Indicator>
